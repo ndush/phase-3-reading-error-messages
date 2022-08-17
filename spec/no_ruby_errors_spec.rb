653a3796f567594bf53878890012109fe81eea39
damaris <mdamarisnduku@gmail.com>
2022-08-17 22:42:23 +0300

    commit tree 6f865c561ecab07e857984feafce2feed1f838c5

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -1,6 +1,6 @@
 describe "Not having any errors and being all green" do
 
-  describe 'NameError' do
+  describe 'NameError'  do
     it 'raises a NameError when encountering undefined barewords' do
       expect { load './lib/a_name_error.rb' }.not_to raise_error
     end
@@ -8,7 +8,7 @@ describe "Not having any errors and being all green" do
 
   describe 'SyntaxError' do
     it 'raises a SyntaxError for nonsensical code' do
-      expect { load './lib/a_syntax_error.rb' }.not_to raise_error
+      expect { 'load' './lib/a_syntax_error.rb' }.not_to raise_error
     end
   end
 
